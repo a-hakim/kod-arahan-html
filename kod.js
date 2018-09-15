5cc3e7f1dd2372a507485331ba171e5007a48e68
A.Hakim Noor <a.hakim.solution@gmail.com>
2018-09-15 18:20:37 +0800

    Baiki kod.js

--- kod.js
+++ kod.js
@@ -1,4 +1,4 @@
-function menu_pautan($id) {
+function menu_jatuh($id) {
 	var urlmenu = document.getElementById($id);
 	urlmenu.onchange = function() {
 		window.open( this.options[ this.selectedIndex ].value );
